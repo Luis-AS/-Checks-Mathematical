7336717522698b4fb107b365e10e0e7f299387b7
Luís Alexandre <53843304+Luis-AS@users.noreply.github.com>
2020-09-17 22:23:38 -0300

    Add files via upload

---
+++ Checks-Mathematical-Expressions-master/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaginaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
